961de0cceabb338d16a13370340a2b76dc27980c
chakrabortysayantan699 <77148703+chakrabortysayantan699@users.noreply.github.com>
2021-01-21 12:48:13 +0530

    Swap.asm
    it swaps contents of two given location

--- swap.asm
+++ swap.asm
@@ -1,7 +1,7 @@
 ;<SWapping content of two location>
-LHLD 0040H
+LHLD 0040H   ;// First data given in  this location
 XCHG
-LHLD 0050H	
+LHLD 0050H	  ;// 2nd data given in  this location
 SHLD 0040H
 XCHG
 SHLD 0050H 	
